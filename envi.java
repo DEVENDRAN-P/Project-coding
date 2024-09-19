1e76e7a20f389c6ad863d48445eac5fe91e07957
DEVENDRAN-P <devendranprabhakar2007@gmail.com>
2024-09-19 18:34:27 +0530

    AWARENESS ABOUT ENVIRONMENT  --- FIRST FILE

---
+++ envi.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class envi{
+    public static void main(String[] args)
+    {
+        
+
+        System.out.println("LET'S WE ARE GOING TO SEE ABOUT ENVIRONMENTAL AWARENESS");
+        System.out.println("WHAT DO YOU THINK ABOUT ENVIRONMENTAL AWARENESS");
+
+
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine(); 
+
+
+        System.out.println("IT'S OKAY ! NICE TRY \n I'LL GIVE YOU SOME VALUABLE INFORMATION ABOUT ENVIRONMENTAL AWARENESS");
+        System.out.println("ENVIRONMENTAL AWARENESS IS CRUCIAL FOR OUR PLANET'S WELL BEING , SOME IMPORTANCE : \n 1.CONSERVATION OF NATURAL RESOURSES \n 2. REDUCTION OF POLLUTION AND WASTE \n 3. PROTECTION OF BIODIVERSITY AND ECOSYSTEMS \n 4. MITIGATION OF CLIMATE CHANGE \n 5. ENSURING A SUSTAINABLE FUTURE ");
+        
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println(" \n \n MAKE YOUR HEART AND EYES SOLID ! BECAUSE NOW IM GOING TO TELL ABOUT EFFECTS OF DEFORESTRATION \n PRESS ANY LETTER TO CONTINUE");
+        String name1 = scan.nextLine();
+        System.out.print("1. LOSS OF BIODIVERSITY (50% OF SPECIES DEPEND ON FORESTS) \n 2. CLIMATE CHANGE \n 3. SOIL EROSION AND LAND SLIDES \n 4. WATER CYCLE DISRUPTION \n 5. INDIGENOUS COMMUNITY DISPLACEMENT");
+        
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println( " \n ENTER ANY NUMBER TO SEE THE SOLUTIONS TO RETAIN HEALTHY ENVIRONMENT");
+        int a = scan2.nextInt();
+
+        System.out.println("1. PLANT MORE SAPLINGS \n 2. CONSERVE WATER AND ENERGY \n 3. AVOID PLASTICS \n 4. MAKE YOUR HOUSE AND YOUR STREET CLEAN \n 5. BE READY BEFORE NATURAL PANDEMICS");
+
+     
+
+
+
+
+
+
+    }
+
+     
+    
+
+
+
+
+    
+
+}
+ 
